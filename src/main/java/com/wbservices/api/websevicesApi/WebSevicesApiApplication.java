c95d8c076474231b6b66c569994124f2f4b53371
Lucas Cardoso Alecrim <115514137+LucasCAlecrim@users.noreply.github.com>
2023-12-17 12:13:11 -0300

    WSSB - Create project on Spring Initializr

---
+++ src/main/java/com/wbservices/api/websevicesApi/WebSevicesApiApplication.java
@@ -0,0 +1,13 @@
+package com.wbservices.api.websevicesApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSevicesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSevicesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wbservices/api/websevicesApi/WebSevicesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wbservices.api.websevicesApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSevicesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
