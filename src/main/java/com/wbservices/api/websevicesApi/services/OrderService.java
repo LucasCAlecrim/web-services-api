e9008127f2b9f0331ff20727aea938da660ffbd8
LucasCAlecrim <lucascardosoalecrim99@gmail.com>
2023-12-17 17:35:24 -0300

    WSSB - Add OrderService.java on service

---
+++ src/main/java/com/wbservices/api/websevicesApi/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.wbservices.api.websevicesApi.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wbservices.api.websevicesApi.model.Order;
+import com.wbservices.api.websevicesApi.repository.OrderRepository;
+
+@Service
+public class OrderService {
+
+    public List<Order> findAll(){
+        return repository.findAll();
+    }
+
+    public Order findById(Long id){
+        Optional<Order> obj = repository.findById(id);
+        return obj.get();
+    }
+
+    @Autowired
+    private OrderRepository repository;
+}
