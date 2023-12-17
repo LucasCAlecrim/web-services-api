73f2ed9bf23a06f3add81d06abb421f2b5dec8af
LucasCAlecrim <lucascardosoalecrim99@gmail.com>
2023-12-17 17:31:00 -0300

    WSSB - Add ProdutoService.java on service

---
+++ src/main/java/com/wbservices/api/websevicesApi/services/ProdutoService.java
@@ -0,0 +1,25 @@
+package com.wbservices.api.websevicesApi.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wbservices.api.websevicesApi.model.Produto;
+import com.wbservices.api.websevicesApi.repository.ProdutoRepository;
+
+@Service
+public class ProdutoService {
+    public List<Produto> findAll() {
+        return repository.findAll();
+    }
+
+    public Produto findById(Long id) {
+        Optional<Produto> obj = repository.findById(id);
+        return obj.get();
+    }
+
+    @Autowired
+    private ProdutoRepository repository;
+}
